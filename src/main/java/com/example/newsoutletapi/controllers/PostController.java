8255a52ff6e672e9a10af4a88e59f29f9c720500
Wudanty <youheixd@gmail.com>
2022-06-07 12:12:18 +0200

    add CORS config for PostController

--- src/main/java/com/example/newsoutletapi/controllers/PostController.java
+++ src/main/java/com/example/newsoutletapi/controllers/PostController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
+@CrossOrigin(methods = {RequestMethod.GET,RequestMethod.DELETE,RequestMethod.POST,RequestMethod.PUT})
 public class PostController {
     @Autowired
     PostService postService;
