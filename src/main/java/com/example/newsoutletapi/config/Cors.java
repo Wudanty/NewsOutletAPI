024c41456fd3455c3c3924850930c55b2bd5c841
Wudanty <youheixd@gmail.com>
2022-12-11 17:18:29 +0100

    next iteration

--- src/main/java/com/example/newsoutletapi/config/Cors.java
+++ src/main/java/com/example/newsoutletapi/config/Cors.java
@@ -14,7 +14,7 @@ public class Cors {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("");
+        config.addAllowedOrigin("http://localhost:3000/");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
--- src/main/java/com/example/newsoutletapi/services/UserService.java
+++ src/main/java/com/example/newsoutletapi/services/UserService.java
@@ -4,17 +4,31 @@ import com.example.newsoutletapi.model.Article;
 import com.example.newsoutletapi.model.User;
 import com.example.newsoutletapi.repos.UserRepository;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
 
+    private PasswordEncoder passwordEncoder;
+
     public UserService(UserRepository userRepository) {
+
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public User getUser(Integer id){
@@ -26,6 +40,8 @@ public class UserService {
     }
 
     public void addUser(User user){
+        String encodedPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         userRepository.save(user);
     }
 
@@ -34,9 +50,10 @@ public class UserService {
     }
 
     public User updateUser(User user){
-        User updatedUser = userRepository.getById(user.getUserId());
+        User updatedUser = userRepository.findById(user.getUserId()).get();
+        String encodedPassword = this.passwordEncoder.encode(user.getPassword());
         updatedUser.setNickname(user.getNickname());
-        updatedUser.setPassword(user.getPassword());
+        updatedUser.setPassword(encodedPassword);
         userRepository.save(updatedUser);
         return updatedUser;
     }
